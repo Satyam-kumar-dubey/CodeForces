d7070848197cd35fb7f7bf2c10e13deab4f67310
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2026-02-19 21:47:30 +0530

    Day - 34 Q1

---
+++ A_Parallelepiped.cpp
@@ -0,0 +1,16 @@
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main ()
+{
+    int x,y,z;
+    cin>>x>>y>>z;
+
+    int a = sqrt((x*z)/y);
+    int b = sqrt((x*y)/z);
+    int c = sqrt((y*z)/x);
+
+    cout<<(4*(a+b+c))<<'\n';
+}

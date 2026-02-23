80d943848bde72f2abcfbe84a205f83b3a29be54
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2026-02-23 21:41:49 +0530

    Day - 38 Q1

---
+++ B_Alternating_Current.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s;
+    cin >> s;
+
+    stack<char> st;
+    for (char c : s) 
+    {
+        if (!st.empty() && st.top() == c) st.pop();  
+        else st.push(c);
+    }
+
+    cout << (st.empty() ? "Yes" : "No") << '\n';
+}

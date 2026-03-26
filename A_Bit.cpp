d7dbb6bf03fb4230a173b6c4e8d2a07655e55381
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2026-03-26 23:13:07 +0530

    Day - 61 Q1

---
+++ A_Bit.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() 
+{
+    int n, x = 0;
+    cin>>n;
+
+    while (n--) 
+    {
+        string s;
+        cin>>s;
+        if(s.find("++") != string::npos) 
+        x++;
+        else x--;
+    }
+    cout<<x;
+}

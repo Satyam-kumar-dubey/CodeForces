7e3b9edc48253f56cd52795a1bd224ed80d268c3
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2026-03-28 23:28:06 +0530

    Day - 62 Q2

---
+++ A_Beautiful_Matrix.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int x,row,col;
+    
+    for (int i=0; i<5; i++) 
+    {
+        for (int j=0; j<5; j++) 
+        {
+            cin>>x;
+            if (x == 1) 
+            {
+                row = i;
+                col = j;
+            }
+        }
+    }
+
+    cout<<abs(row - 2) + abs(col - 2);
+}

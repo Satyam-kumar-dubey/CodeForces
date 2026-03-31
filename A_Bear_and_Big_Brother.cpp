748a1d3819503ba4e7bcfad816dbc00a94b8470e
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2026-04-01 00:05:30 +0530

    Day - 66 Q2

---
+++ A_Bear_and_Big_Brother.cpp
@@ -0,0 +1,19 @@
+
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int a, b, yr = 0;
+    cin>>a>>b;
+
+    while(a <= b)
+    {
+        a *= 3;
+        b *= 2;
+        yr++;
+    }
+    cout<<yr;
+}

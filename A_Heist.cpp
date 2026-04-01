01f7a06d9c3c71af6c6761d8333e3f2fd8dd7d0f
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2026-04-01 13:26:39 +0530

    Day - 68 Q3

---
+++ A_Heist.cpp
@@ -0,0 +1,23 @@
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin>>n;
+    
+    vector<int>v(n);
+    for(int i=0; i<n; i++)
+    cin >> v[i];
+
+    int mn = *min_element(v.begin(), v.end());
+    int mx = *max_element(v.begin(), v.end());
+
+    cout<<mx-mn+1-n;
+}

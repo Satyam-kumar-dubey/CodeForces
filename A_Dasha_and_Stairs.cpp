fdace3891bb4ccc9bd86f5ea2854110173562c16
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2026-04-03 22:38:05 +0530

    Day - 70 Q2

---
+++ A_Dasha_and_Stairs.cpp
@@ -0,0 +1,17 @@
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int a,b;
+    cin>>a>>b;
+
+    if((a + b > 0) && (abs(a - b) <= 1))
+    cout << "YES";
+    else
+    cout << "NO";
+}

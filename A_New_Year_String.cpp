94b8ff7f0f3bc8c5b06452db806979f8f23a6c85
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2026-04-07 00:37:26 +0530

    Day - 71 Q1

--- A_New_Year_String.cpp
+++ A_New_Year_String.cpp
@@ -1,34 +1,36 @@
-#include <iostream>
-#include <string>
+
+#include<iostream>
+#include<string>
 using namespace std;
 
-int newyear(const string& n)
+int solve(const string& s)
 {
-    int id1 = n.find("2026");
-    int id2 = n.find("2025");
+    int f = s.find("2026");
+    int sec = s.find("2025");
 
-    if (id1 != string::npos || id2 == string::npos)
-        return 0;
+    if(f != string::npos || sec == string::npos)
+    return 0;
 
     return 1;
 }
+
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    int t;
+    cin>>t;
 
-    int tests;
-    cin >> tests;
-
-    while (tests--)
+    while(t--)
     {
-        int m;
-        cin >> m;
-        cin.ignore();          
+        int n;
+        cin>>n;
+
+        cin.ignore(); 
+
         string s;
         getline(cin, s);
-
-        cout << newyear(s) << "\n";
+        cout<<solve(s)<<"\n";
     }
 }

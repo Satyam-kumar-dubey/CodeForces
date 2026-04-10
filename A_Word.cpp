c7adf42b0f2a70bca53824dd3228e4259599940d
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2026-04-10 22:23:18 +0530

    Day - 75 Q4

--- A_Word.cpp
+++ A_Word.cpp
@@ -1,48 +1,35 @@
-
 #include<iostream>
-#include<cctype>
 #include<string>
+#include<cctype>
 using namespace std;
 
-int main ()
+int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
     string s;
     cin>>s;
 
-    int upper = 0;
-    int lower = 0;
-    
-    for(int i=0; i<s.size(); i++)
-    {
-        char ch = s[i];
-        if(int(ch)>=65 && int(ch)<=90)
-        upper++;
-        else
-        lower++;
-    }
-    if(lower == upper)
-    {
-        for(char &c: s)
-        c = tolower(c);
+    int u=0,l=0;
 
-        cout<<s;
-    }
-    else if(lower > upper)
+    for (char c : s)
     {
-        for(char &c: s)
-        {
-            if(int(c)>=65 && int(c)<=90)
-            c = tolower(c);
-        }
-        cout<<s;
+        if (isupper(c))
+        u++;
+        else
+        l++;
     }
+
+    if (u>l)
+    {
+        for(char &c : s)
+        c = toupper(c);
+    } 
     else
     {
-        for(char &c: s)
-        {
-            if(int(c)>=97 && int(c)<=122)
-            c = toupper(c);
-        }
-        cout<<s;
+        for(char &c : s)
+        c = tolower(c);
     }
+
+    cout<<s;
 }

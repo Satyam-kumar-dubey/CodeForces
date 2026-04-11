3429ce8e54ed33b9bc2feed0e5b51648e6deed4f
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2026-04-12 00:39:53 +0530

    Day - 79 Q1

--- A_Codeforces_Checking.cpp
+++ A_Codeforces_Checking.cpp
@@ -1,29 +1,23 @@
-#include <iostream>
+#include<iostream>
+#include<string>
 using namespace std;
 
-int main() {
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int t;
-    cin >> t;
-
-    string s = "codeforces";
-
-    while (t--) {
+    cin>>t;
+    string s="codeforces";
+    while(t--)
+    {
         char c;
-        cin >> c;
+        cin>>c;
 
-        bool found = false;
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] == c) {
-                found = true;
-                break;
-            }
-        }
-
-        if (found)
-            cout << "YES\n";
+        if(s.find(c) != string::npos)
+        cout<<"YES"<<"\n";
         else
-            cout << "NO\n";
+        cout<<"NO"<<"\n";
     }
-
-    return 0;
 }

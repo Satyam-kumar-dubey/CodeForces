be7c9e734f8a03aa93b2290d4628ac0c3924b9e5
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2026-04-12 00:34:35 +0530

    Day - 78 Q4

--- A_Doremy_s_Paint_3.cpp
+++ A_Doremy_s_Paint_3.cpp
@@ -1,46 +1,44 @@
-
 #include<iostream>
-#include<vector>
-#include<unordered_map>
+#include<map>
+#include<cmath>
+#include<algorithm>
 using namespace std;
 
-int main ()
+int main()
 {
-    int test;
-    cin>>test;
-    while(test--)
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin>>t;
+
+    while(t--)
     {
         int n;
         cin>>n;
-        vector<int>v(n);
-        for(int i=0; i<n; i++)
-        cin>>v[i];
 
-        unordered_map<int,int>m;
-        for(int val : v)
-        m[val]++;
+        map<int,int> m;
+        for (int i=0; i<n; i++)
+        {
+            int x;
+            cin >> x;
+            m[x]++;
+        }
 
-        if(m.size()>2)
-        {
-            cout<<"No"<<endl;
-            continue;
-        }
-        if(m.size()==1)
-        {
-            cout<<"Yes"<<endl;
-            continue;
-        }
-        int first = 0,second = 0;
-        for(auto &i : m)
-        {
-            if(!first)
-            first = i.second;
-            else
-            second = i.second;
-        }
-        if(abs(first - second) > 1)
-        cout<<"No"<<endl;
+        if(m.size() == 1)
+        cout << "Yes\n";
+        else if(m.size() > 2)
+        cout<<"No\n";
         else
-        cout<<"Yes"<<endl;
+        {
+            auto i = m.begin();
+            int c1 = i->second;
+            ++i;
+            int c2 = i->second;
+            if(abs(c1 - c2) <= 1)
+            cout<<"Yes"<<"\n";
+            else
+            cout<<"No"<<"\n";
+        }
     }
 }

7738e96660e7cda42d41dc53bb4a4374e5a11347
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2026-04-11 20:36:50 +0530

    Day - 78 Q1

--- A_Cover_in_Water.cpp
+++ A_Cover_in_Water.cpp
@@ -1,36 +1,42 @@
 #include<iostream>
 #include<string>
-#include<algorithm>
 using namespace std;
 
 int main()
 {
-    int test;
-    cin>>test;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    while(test--)
+    int t;
+    cin>>t;
+
+    while(t--)
     {
         int n;
         cin>>n;
         string s;
         cin>>s;
 
-        int total = 0,maxi = 0,i = 0;
-        while(i<s.size())
+        bool x = false;
+        for (int i = 0; i + 2 < n; i++)
         {
-            if(s[i]=='.')
+            if (s[i] == '.' && s[i + 1] == '.' && s[i + 2] == '.')
             {
-                int count = 0;
-                while(i<s.size() && s[i] != '#')
-                {
-                    count++;
-                    i++;
-                }
-                maxi = max(maxi,count);
-                total += count;
+                x=true;
+                break;
             }
-            i++;
         }
-        cout<<(maxi >= 3 ? 2: total)<<endl;
+        if(x)
+        cout<<2<<'\n';
+        else
+        {
+            int t=0;
+            for (char c : s)
+            {
+                if (c == '.')
+                t++;
+            }
+            cout<<t<<'\n';
+        }
     }
 }

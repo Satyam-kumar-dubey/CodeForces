95a217cd2a1895ed42c67de4f96022d2faa7438b
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2026-04-11 11:55:11 +0530

    Day - 76 Q3

--- A_Anton_and_Danik.cpp
+++ A_Anton_and_Danik.cpp
@@ -10,22 +10,23 @@ int main ()
 
     int n;
     cin>>n;
+
     string s;
     cin>>s;
+    int a=0, d=0;
 
-    int A_count = 0, D_count = 0;
-    for(char c : s)
+    for(char c: s)
     {
         if(c == 'A')
-        A_count++;
+        a++;
         else
-        D_count++;
+        d++;
     }
-
-    if(A_count == D_count)
-    cout<<"Friendship"<<'\n';
-    else if ( A_count > D_count)
+    
+    if(a > d)
     cout<<"Anton"<<'\n';
-    else
+    else if(d > a)
     cout<<"Danik"<<'\n';
+    else
+    cout<<"Friendship"<<'\n';
 }

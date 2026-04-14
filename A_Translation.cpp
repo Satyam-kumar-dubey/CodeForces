26badf999665e5dcd3cb0a56d9c7381843c48b1c
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2026-04-14 22:42:56 +0530

    Day - 85 Q1

--- A_Translation.cpp
+++ A_Translation.cpp
@@ -1,28 +1,19 @@
 
 #include<iostream>
-#include<string>
+#include<algorithm>
 using namespace std;
 
-int main ()
+int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     string s,t;
     cin>>s>>t;
+    reverse(s.begin(), s.end());
 
-    if(s.size() != t.size())
-    {
-        cout<<"NO"<<'\n';
-        return 0;
-    }
-    int first = 0, second = t.size()-1;
-    while(first < s.size())
-    {
-        if(s[first] != t[second])
-        {
-            cout<<"NO"<<'\n';
-            return 0;
-        }
-        first++;
-        second--;
-    }
-    cout<<"YES"<<'\n';
+    if(s==t)
+    cout<<"YES";
+    else
+    cout<<"NO";
 }

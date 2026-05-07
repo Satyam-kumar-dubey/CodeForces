ff8c6412cc7122c22c9518f8fa141f301dafee66
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2026-05-07 15:06:05 +0530

    Day - 112 Q2

---
+++ B_Alternating_Series.cpp
@@ -0,0 +1,35 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        int n;
+        cin>>n;
+
+        if(n == 2)
+        cout<<-1<<" "<<2<<'\n';
+        else
+        {
+            vector<int>v(n);
+            for(int i=0; i<n; i++)
+            {
+                if(i % 2 == 0)
+                v[i] = -1;
+                else 
+                v[i] = 3;
+            }
+            for(int x : v)
+            cout<<x<<" ";
+            cout<<'\n';
+        }
+    }
+}

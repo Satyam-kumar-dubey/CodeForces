29141035e0aef68ccda0542d25ef0fd59e821dfe
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2026-05-07 16:33:20 +0530

    Day - 113 Q1

---
+++ B_Blank_Space.cpp
@@ -0,0 +1,34 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<int>v(n);
+
+        for(int i=0; i<n; i++)
+        cin>>v[i];
+
+        int a = 0, c=0;
+        for(int x: v)
+        {
+            if(x == 0)
+            c++;
+            else
+            c = 0;
+
+            a = max(a,c);
+        }
+        cout<<a<<"\n";
+    }
+}

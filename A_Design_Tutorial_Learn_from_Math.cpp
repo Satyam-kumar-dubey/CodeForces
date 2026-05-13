e3a15533404adf328684b5d1f72080e89ea5dd67
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2026-05-13 21:12:27 +0530

    Day - 121 Q2

---
+++ A_Design_Tutorial_Learn_from_Math.cpp
@@ -0,0 +1,17 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin>>n;
+
+    if(n % 2 == 0)
+    cout<<4<<" "<<(n-4)<<'\n';
+    else
+    cout<<9<<" "<<(n-9)<<'\n';
+}

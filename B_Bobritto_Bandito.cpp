2b26e7eebd19d8c1ccd4d7b6ff2563940d0e4124
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2026-05-17 18:13:37 +0530

    Day - 131 Q2

---
+++ B_Bobritto_Bandito.cpp
@@ -0,0 +1,23 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t; 
+    cin>>t;
+
+    while(t--)
+    {
+        int n,m,l,r;
+        cin>>n>>m>>l>>r;
+
+        int nl = min(m,abs(l));
+        int nr = m - nl;
+
+        cout<<-1*(nl)<<" "<<nr<<'\n';
+    }
+}

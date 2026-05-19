99e9b4c0a0579c752dbe0933e18c384099143dcb
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2026-05-19 16:24:26 +0530

    Day - 139 Q1

--- A_George_and_Accommodation.cpp
+++ A_George_and_Accommodation.cpp
@@ -1,6 +1,5 @@
 
-#include<iostream>
-#include<vector>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main ()
@@ -8,17 +7,17 @@ int main ()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int size;
-    cin>>size;
+    int n;
+    cin>>n;
 
-    int count = 0;
-    vector<pair<int,int>>arr(size);
-    for(int i=0; i<size; i++)
+    int c = 0;
+    for(int i=0; i<n; i++)
     {
-        cin>>arr[i].first>>arr[i].second;
-        if((arr[i].second - arr[i].first) >= 2)
-        count++;
+        int a,b;
+        cin>>a>>b;
+        if((b - a) >= 2)
+        c++;
     }
 
-    cout<<count<<'\n';
+    cout<<c<<'\n';
 }

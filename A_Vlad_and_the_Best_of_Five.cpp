74288da6c30586b3789cd9d8bd96830b8b9cdb0e
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2026-05-21 12:08:44 +0530

    Day - 147 Q3

---
+++ A_Vlad_and_the_Best_of_Five.cpp
@@ -0,0 +1,29 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        string s;
+        cin>>s;
+
+        int a = 0, b = 0;
+        for(char c : s)
+        {
+            if(c == 'A')
+            a++;
+            else 
+            b++;
+        }
+        cout<<(a > b ? 'A' : 'B')<<'\n';
+    }
+    
+}

824060aae3dfc07e1b7bbc4d2884c96e0704e85e
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2026-05-23 11:47:39 +0530

    Day - 162 Q3

---
+++ B_Atilla_s_Favorite_Problem.cpp
@@ -0,0 +1,25 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+int main ()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        int n;
+        string s;
+        cin>>n>>s;
+
+        sort(s.begin(),s.end());
+        cout<<(s[n-1]-'a')+1<<'\n';
+    }
+    
+}

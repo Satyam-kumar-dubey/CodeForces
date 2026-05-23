1446d3d9af40d0f993d06c45c605909eb018203d
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2026-05-23 10:05:05 +0530

    Day - 160 Q1

--- A_Uniform_String.cpp
+++ A_Uniform_String.cpp
@@ -1,6 +1,5 @@
 
-#include<iostream>
-#include<string>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main ()
@@ -8,20 +7,20 @@ int main ()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int test;
-    cin>>test;
+    int t;
+    cin>>t;
 
-    while(test--)
+    while(t--)
     {
         int n,k;
         cin>>n>>k;
 
-        string ans;
+        string a;
         for(int i=0; i<n; i++)
         {
             char ch = ('a' + (i%k));
-            ans += ch;
+            a += ch;
         }
-        cout<<ans<<'\n';
+        cout<<a<<'\n';
     }
 }

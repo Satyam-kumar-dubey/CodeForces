3869cede0564480ae988a12bd06c9251f8e1672c
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2026-05-23 09:51:59 +0530

    Day - 157 Q2

--- A_Dima_and_Friends.cpp
+++ A_Dima_and_Friends.cpp
@@ -1,6 +1,5 @@
 
-#include<iostream>
-#include<vector>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main ()
@@ -12,14 +11,14 @@ int main ()
     for(int i=0; i<n; i++)
     cin>>v[i];
 
-    int sum = 0, count = 0;
+    int s = 0, c = 0;
     for(int val : v)
-    sum += val;
+    s += val;
 
     for(int i=1; i<=5; i++)
     {
-        if((sum + i) % (n+1) != 1)
-        count++;
+        if((s + i) % (n+1) != 1)
+        c++;
     }
-    cout<<count<<'\n';
+    cout<<c<<'\n';
 }

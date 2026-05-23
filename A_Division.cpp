64ad0154796332a18f218165b3213454afda1c41
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2026-05-23 10:20:35 +0530

    Day - 161 Q1

---
+++ A_Division.cpp
@@ -0,0 +1,30 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+int main ()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        int n;
+        cin>>n;
+
+        if(n <= 1399)
+        cout<<"Division 4"<<'\n';
+        if(n >= 1400 && n <= 1599)
+        cout<<"Division 3"<<'\n';
+        if(n >= 1600 && n <= 1899)
+        cout<<"Division 2"<<'\n';
+        if(n >= 1900)
+        cout<<"Division 1"<<'\n';
+    }
+    
+}

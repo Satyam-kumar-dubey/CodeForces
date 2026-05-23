e92f6d6ede0d9497493104248c7d855d9aa908ea
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2026-05-23 09:58:16 +0530

    Day - 159 Q1

--- A_Parallelepiped.cpp
+++ A_Parallelepiped.cpp
@@ -1,10 +1,12 @@
 
-#include<iostream>
-#include<cmath>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main ()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
     int x,y,z;
     cin>>x>>y>>z;
 

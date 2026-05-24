bcad2dae2ebb58688a3ff62a57faabad22f78056
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2026-05-24 09:56:11 +0530

    Day - 166 Q2

---
+++ A_Raising_Bacteria.cpp
@@ -0,0 +1,18 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+int main ()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n;
+    cin>>n;
+
+    int c = __builtin_popcount(n);
+    cout<<c<<'\n';
+    
+}

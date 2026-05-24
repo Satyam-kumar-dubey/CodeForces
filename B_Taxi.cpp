25202d8e215f090d6b7dba0dd392e691cfb12388
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2026-05-24 21:46:46 +0530

    Day - 171 Q1

--- B_Taxi.cpp
+++ B_Taxi.cpp
@@ -2,36 +2,47 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-using ll = long long;
-
 int main ()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+
     int n;
     cin>>n;
 
-    vector<int>v(n);
-    for(int i=0; i<n; i++)
-    cin>>v[i];
+    int o = 0, tw = 0, th = 0, fr = 0;
 
-    sort(v.begin(),v.end());
-
-    int s = 0, e = n-1, c = 0;
-    while(s <= e)
+    for(int i = 0; i < n; i++)
     {
-        if(v[s] + v[e] > 4)
-        {
-            c++;
-            e--;
-        }
-        else 
-        {
-            c++;
-            s++;
-            e--;
-        }
+        int x;
+        cin >> x;
+
+        if(x == 1)
+        o++;
+        else if(x == 2)
+        tw++;
+        else if(x == 3)
+        th++;
+        else
+        fr++;
     }
-    cout<<c<<"\n";
+
+    int a = 0;
+    a += fr;
+
+    int pair31 = min(o, th);
+    a += th;
+    o -= pair31;
+
+    a += tw / 2;
+
+    if(tw % 2)
+    {
+        a++;
+        o = max(0, o - 2);
+    }
+
+    a += (o + 3) / 4;
+
+    cout<<a<<'\n';
 }

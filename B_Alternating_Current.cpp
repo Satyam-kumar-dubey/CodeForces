00ccc6d5ca4859d90e4934e009be75e1e0fc79f1
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2026-05-27 16:13:07 +0530

    Day - 182 Q1

--- B_Alternating_Current.cpp
+++ B_Alternating_Current.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
-#include <stack>
+
+#include<bits/stdc++.h>
 using namespace std;
 
 int main() {
@@ -7,14 +7,16 @@ int main() {
     cin.tie(nullptr);
 
     string s;
-    cin >> s;
+    cin>>s;
 
-    stack<char> st;
+    stack<char>st;
     for (char c : s) 
     {
-        if (!st.empty() && st.top() == c) st.pop();  
-        else st.push(c);
+        if (!st.empty() && st.top() == c)
+        st.pop();  
+        else
+        st.push(c);
     }
 
-    cout << (st.empty() ? "Yes" : "No") << '\n';
+    cout<<(st.empty() ? "Yes" : "No")<<'\n';
 }

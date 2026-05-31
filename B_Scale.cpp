51817194cd2510ec21f365dc1693fdd154c7f34b
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2026-05-31 19:40:12 +0530

    Day - 196 Q1

---
+++ B_Scale.cpp
@@ -0,0 +1,33 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+int main ()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        int n,k;
+        cin>>n>>k;
+
+        vector<string>v(n);
+        for(int i=0; i<n; i++)
+        cin>>v[i];
+
+        for(int i=0; i<n; i=i+k)
+        {
+            for(int j=0; j<n; j=j+k)
+            cout<<v[i][j];
+
+            cout<<'\n';
+        }
+    }
+    
+}

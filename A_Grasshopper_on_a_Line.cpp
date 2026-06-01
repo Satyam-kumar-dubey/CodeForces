8a038f0fd9e4bea1ff2f85f525e77cc9e1627fc8
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2026-06-01 19:12:09 +0530

    Day - 198 Q1

---
+++ A_Grasshopper_on_a_Line.cpp
@@ -0,0 +1,32 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+int main ()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        int n,k;
+        cin>>n>>k;
+
+        if(n % k != 0)
+        {
+            cout<<1<<'\n';
+            cout<<n<<'\n';
+        }
+        else
+        {
+            cout<<2<<'\n';
+            cout<<(n-1)<<" "<<1<<'\n';
+        }
+    }
+    
+}

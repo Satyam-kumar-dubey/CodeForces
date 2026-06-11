521b8133a704a3ec2028ed076b31c01f4562e94d
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2026-06-11 21:05:04 +0530

    Day - 220 Q2

--- D_Harder_Problem.cpp
+++ D_Harder_Problem.cpp
@@ -18,8 +18,38 @@ int main ()
         cin>>n;
 
         vector<int>v(n);
+        set<int>s;
         for(int i=0; i<n; i++)
-        cin>>v[i];
+        {
+            cin>>v[i];
+            s.insert(v[i]);
+        }
+
+        vector<int>np;
+        for(int i=1; i<=n; i++)
+        {
+            if(!s.count(i))
+            np.push_back(i);
+        }
+
+        vector<int>a;
+        map<int,int>m;
+        for(int i=0; i<n; i++)
+        {
+            if(!m.count(v[i]))
+            {
+                a.push_back(v[i]);
+                m[v[i]] = 1;
+            }
+            else
+            {
+                a.push_back(np.back());
+                np.pop_back();
+            }
+        }
+        for(auto &it : a)
+        cout<<it<<" ";
+        cout<<'\n';
     }
     
 }

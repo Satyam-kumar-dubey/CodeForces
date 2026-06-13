91c76dbec8511e88c9fb1b1d0f91b725c8922699
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2026-06-13 12:20:28 +0530

    Day - 224 Q1

--- A_Square.cpp
+++ A_Square.cpp
@@ -9,12 +9,15 @@ int main()
 
     while (t--)
     {
-        int a,b,c,d;
-        cin>>a>>b>>c>>d;
+        int x1,y1,x2,y2,x3,y3,x4,y4;
+        cin>>x1>>y1>>x2>>y2>>x3>>y3>>x4>>y4;
 
-        if (a == b && b == c && c == d)
-        cout << "YES"<<"\n";
-        else
-        cout << "NO"<<"\n";
+        int mx = max({x1,x2,x3,x4});
+        int my = max({y1,y2,y3,y4});
+        int mnx = min({x1,x2,x3,x4});
+        int mny = min({y1,y2,y3,y4});
+
+        int s = min(mx-mnx, my-mny);
+        cout<<s*s<<'\n';
     }
 }

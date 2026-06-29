7eb38e719a60ddbd0ed7253844419606437db860
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2026-06-29 12:43:05 +0530

    Day - 247 Q1

---
+++ A_Mahmoud_and_Ehab_and_the_even_odd_game.cpp
@@ -0,0 +1,16 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+int main ()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n;
+    cin>>n;
+
+    cout<<(n % 2 == 0 ? "Mahmoud" : "Ehab")<<'\n';
+}

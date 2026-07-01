f7a562701200e0a115fd64c4363009140b4819f6
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2026-07-01 17:36:19 +0530

    Day - 252 Q2

--- B_Good_times_Good_times.cpp
+++ B_Good_times_Good_times.cpp
@@ -1,82 +1,40 @@
+
 #include<bits/stdc++.h>
 using namespace std;
 
 using ll = long long;
 
-vector<ll> good;
-
-bool check(ll x)
+void in(vector<int>&v)
 {
-    int mask = 0;
-
-    while(x)
-    {
-        mask |= (1 << (x % 10));
-        x /= 10;
-    }
-
-    return __builtin_popcount(mask) <= 2;
-}
-
-void gen(int len, int mx, int d1, int d2, ll cur)
-{
-    if(len > 0 && cur >= 2 && cur <= 1000000000LL)
-        good.push_back(cur);
-
-    if(len == mx)
-        return;
-
-    if(len > 0 || d1 != 0)
-        gen(len + 1, mx, d1, d2, cur * 10 + d1);
-
-    if(d1 != d2)
-    {
-        if(len > 0 || d2 != 0)
-            gen(len + 1, mx, d1, d2, cur * 10 + d2);
-    }
-}
-
-void pre()
-{
-    for(int i = 0; i <= 9; i++)
-    {
-        for(int j = i; j <= 9; j++)
-        {
-            gen(0, 9, i, j, 0);
-        }
-    }
-
-    sort(good.begin(), good.end());
-    good.erase(unique(good.begin(), good.end()), good.end());
+    for(auto &x: v)
+    cin>>x;
 }
 
 void solve()
 {
-    ll x;
-    cin >> x;
+    int n;
+    cin>>n;
 
-    for(auto y : good)
+    int y = 1;
+    while(n > 0)
     {
-        if(check(x * y))
-        {
-            cout << y << '\n';
-            return;
-        }
+        y *= 10;
+        n /= 10;
     }
+    cout<<y+1<<'\n';
 }
 
-int main()
+int main ()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
-    pre();
-
+    
     int t;
-    cin >> t;
+    cin>>t;
 
     while(t--)
+    {
         solve();
-
-    return 0;
+    }
+    
 }

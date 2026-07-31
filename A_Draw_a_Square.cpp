c35f2db55a24dc983b01ac7eee81f5d0693cd1ba
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2026-07-31 10:39:34 +0530

    Day - 274 Q2

---
+++ A_Draw_a_Square.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+void in(vector<int>&v)
+{
+    for(auto &x: v)
+    cin>>x;
+}
+bool prime(ll n)
+{
+    if(n <= 1)
+    return false;
+    for(ll i=2; i*i <= n; i++)
+    {
+        if(n%i == 0)
+        return false;
+    }
+    return true;
+}
+
+void solve()
+{
+    int l,r,d,u;
+    cin>>l>>r>>d>>u;
+
+    cout<<(l==r && r==d && d==u ? "Yes" : "No")<<'\n';
+}
+
+int main ()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        solve();
+    }
+    
+}

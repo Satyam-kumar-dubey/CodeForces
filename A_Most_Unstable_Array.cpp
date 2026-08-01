c39f438b7bc62112b0511be59cf9a2d7de476550
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2026-08-01 23:37:47 +0530

    Day - 276 Q1

---
+++ A_Most_Unstable_Array.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+void in(vector<int>&v)
+{
+    for(auto &x: v)
+    cin>>x;
+}
+bool prime(ll n)
+{
+    if(n <= 1)
+    return false;
+    for(ll i=2; i*i <= n; i++)
+    {
+        if(n%i == 0)
+        return false;
+    }
+    return true;
+}
+
+void solve()
+{
+   ll n, m;
+    cin >> n >> m;
+
+    if (n == 1)
+    cout << 0 << "\n";
+    else if (n == 2)
+    cout << m << "\n";
+    else
+    cout << 2 * m << "\n";
+}
+
+int main ()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        solve();
+    }
+    
+}

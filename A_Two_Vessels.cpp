ce3ce52318478f9d4bdecd00af66c2a2c67488ee
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2026-08-12 22:58:22 +0530

    Day - 284 Q1

---
+++ A_Two_Vessels.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+void in(vector<int>&v)
+{
+    for(auto &x: v)
+    cin>>x;
+}
+bool prime(ll n)
+{
+    if(n <= 1)
+    return false;
+    for(ll i=2; i*i <= n; i++)
+    {
+        if(n%i == 0)
+        return false;
+    }
+    return true;
+}
+
+void solve()
+{
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        int diff = abs(a - b);
+
+        cout << (diff + 2 * c - 1) / (2 * c) << '\n';
+}
+
+int main ()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        solve();
+    }
+    
+}
